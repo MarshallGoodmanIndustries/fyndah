99ba26585804fd714005e0d1791810477828671f
Daisy Dawn <agboella20@gmail.com>
2024-06-05 04:11:27 +0100

    done with the profile settings/display page

--- src/components/businessDashboard/BusinessDashboardLayout.jsx
+++ src/components/businessDashboard/BusinessDashboardLayout.jsx
@@ -11,14 +11,14 @@ const BusinessDashboardLayout = () => {
   }
 
   return (
-    <div className='admin-layout font-inter'>
-      <section className='business-header'>
+    <div className='business-layout w-full font-roboto'>
+      <section className='business-header w-full'>
         <Header toggle={toggle} handleToggle={handleToggle} />
       </section>
         <section className={`business-sidebar w-[250px] z-20 duration-500 ease-in-out sm:block ${toggle ? 'block' : 'hidden'}`}>
           <SideBar handleToggle={handleToggle} toggle={toggle} />
         </section>
-        <section className='business-outlet w-screen'>
+        <section className='business-outlet w-full md:w-auto'>
         <Outlet />
         </section>
         
