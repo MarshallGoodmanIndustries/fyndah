6d887928c68afcf3a44407d352644cb49ec0ce5a
FBK-Manuel <emmanuelbigboss26@gmail.com>
2024-06-05 10:19:10 +0100

    wallet

--- src/components/wallet/welletComponent/WalletIndex.jsx
+++ src/components/wallet/welletComponent/WalletIndex.jsx
@@ -4,16 +4,19 @@ import { FaRegChartBar, FaWallet } from "react-icons/fa"
 import { MdPayment } from "react-icons/md"
 import { useNavigate } from "react-router-dom";
 import Modal from "./Modal";
-import { useEffect, useState } from "react";
+import { useContext, useEffect, useState } from "react";
 import axios from "axios";
 import PayLeadsForm from "./PayLeadsForm";
-import Flutterwave from "./Flutterwave";
+import { AuthContext } from "../../context/AuthContext";
 // import Flutterwave from "./Flutterwave";
 // import Flutterwave from "./Flutterwave";
+// import Flutterwave from "./Flutterwave";
+
 
 function WalletIndex() {
     const navigate = useNavigate();
     const [lowBal, setLowBal] = useState(false);
+    // const [isLoading, setIsLoading] = useState(true)
 
     // for adding funds to wallet v
     const [isModalOpen, setIsModalOpen] = useState(false);
@@ -27,19 +30,37 @@ function WalletIndex() {
     const [addAmountWallet, setAddAmountWallet] = useState('')
     const [payLeadsForm, setPayLeadsForm] = useState('')
 
+    const { authToken } = useContext(AuthContext)
     // for the first modal to flutterwave
     const redirectToFlutterwave = async () => {
         setIsModalOpen(false);
-        navigate();
+        navigate('');
+
+
+
         // for the payment
-        // try {
-        //     const payment = await axios.post('https://api.fyndah.com/api/v1/organization/flutterwave/pay')
-        //     console.log(payment.data);
-        // } catch (error) {
-        //     console.log(error);
-        // }
+        try {
+            // const token = localStorage.getItem('authToken')
+
+            const details = {
+                organization_id: 'devin',
+                amount: addAmountWallet,
+                user_id: 1
+
+            }
+            const payment = await axios.post('https://api.fyndah.com/api/v1/organization/flutterwave/pay', details, {
+                headers: {
+                    'Accept': 'application/json',
+                    'Content-Type': 'multipart/form-data',
+                    Authorization: `Bearer ${authToken}`
+                }
+            })
+            console.log(payment.data);
+        } catch (error) {
+            console.log(error.message);
+        }
         // console.log(addAmountWallet, "from walletindex")
-        <Flutterwave />
+        // <Flutterwave />
     };
     // this for the second modal to pay for leads
     const redirectToPayLeadsForm = async () => {
@@ -59,19 +80,31 @@ function WalletIndex() {
     const [startDate, setStartDate] = useState('');
     const [endDate, setEndDate] = useState('');
     const [transactions, setTransactions] = useState([]);
+    const orgId = "devin";
     useEffect(() => {
-        // Function to fetch transaction data from API
-        const fetchTransactions = async () => {
+        const fetchWalletBalance = async () => {
             try {
-                const response = await axios.post('https://api.fyndah.com/api/v1/organization/wallet/balance');
-                setTransactions(response.data);
-                console.log(response.data) // Update transactions state with API data
+                const response = await axios.post(
+                    'https://api.fyndah.com/api/v1/organization/wallet/balance',
+                    null,
+                    {
+                        headers: {
+                            Accept: 'application/json',
+                        },
+                        params: {
+                            org_id: orgId,
+                        },
+                    }
+                );
+                setTransactions(response.data.balance);
+                console.log(response.data)// Assuming the balance is available under `response.data.balance`
             } catch (error) {
-                console.error('Error fetching transactions:', error);
+                console.error("Error fetching wallet balance:", error);
             }
         };
-        fetchTransactions(); // Call the fetchTransactions function when the component mounts
-    }, []);
+
+        fetchWalletBalance();
+    }, [orgId]);
 
 
     const filterTransactions = () => {
@@ -96,8 +129,8 @@ function WalletIndex() {
 
     return (
         <div>
-            <div className="flex flex-col items-center min-h-screen bg-gray-100 p-6">
-                <div className="w-full max-w-xl mt-5 p-4 bg-white rounded-lg shadow-md">
+            <div className="flex-1 flex flex-col items-center p-4 sm:p-6 md:p-10">
+                <div className="w-full max-w-md sm:max-w-lg md:max-w-2xl mt-5 p-4 bg-white rounded-lg shadow-md">
                     <div className="flex flex-col sm:flex-row items-center justify-between">
                         <h2 className="text-gray-600">Hello,</h2>
                         <h2 className="text-gray-600">Bemia Johnson</h2>
@@ -106,38 +139,86 @@ function WalletIndex() {
                         <h3 className="text-xl">Total Balance</h3>
                         <p className="text-2xl font-bold" onChange={lowBalance}>$8,458.00</p>
                     </div>
-                    <div className="mt-4 flex flex-col sm:flex-row sm:justify-between space-y-4 sm:space-y-0">
-                        <button className="flex flex-col items-center" onClick={openModal}>
+                    <div className="mt-4 flex sm:flex-row justify-center sm:space-y-0 ">
+                        <button className="flex flex-col mr-3  items-center" onClick={openModal}>
                             <MdPayment className="p-2 bg-blue-200 hover:text-blue-800 text-blue-500 rounded-full text-4xl" />
                             <span className="text-sm mt-1">Add Funds</span>
                         </button>
-                        <button className="flex flex-col items-center" onClick={openModal2}>
+                        <button className="flex flex-col items-center mr-5" onClick={openModal2}>
                             <FaWallet className="p-2 bg-red-200 hover:text-red-800 text-red-500 rounded-full text-4xl" />
                             <span className="text-sm mt-1">Purchase Leads</span>
                         </button>
                         <button className="flex flex-col items-center">
                             <FaRegChartBar className="p-2 bg-green-200 hover:text-green-800 text-green-500 rounded-full text-4xl" />
-                            <span className="text-sm mt-1">Stats</span>
+                            <span className="text-sm mt-1">Statistics</span>
                         </button>
                     </div>
                     <div className="mt-6">
-                        <h3 className="text-lg font-bold">Transactions history <hr className="p-2" /></h3>
-                        <div className="flex flex-col sm:flex-row sm:justify-between space-y-4 sm:space-y-0">
-                            <div className="flex items-center">
-                                <label htmlFor="startDate" className="font-bold mr-1">Start Date:</label>
-                                <input type="date" id="startDate" value={startDate} onChange={handleChangeStartDate} className="border rounded-md p-2" />
-                            </div>
-                            <div className="flex items-center">
-                                <label htmlFor="endDate" className="font-bold mr-1">End Date:</label>
-                                <input type="date" size={10} id="endDate" value={endDate} onChange={handleChangeEndDate} className="border rounded-md p-2" />
-                            </div>
+                        <div className="flex sm:flex-row  sm:space-y-0">
+                            <h3 className="text-lg font-bold">Transactions history </h3>
                         </div>
+                        <hr className="p-2" />
+                        <div className="flex sm:flex-row justify-center  sm:space-y-0">
+                            <div className="flex items-center mr-3">
+                                <label htmlFor="startDate" className="font-bold text-sm mr-1">Start Date:</label>
+                                <input
+                                    type="date"
+                                    id="startDate"
+                                    value={startDate}
+                                    onChange={handleChangeStartDate}
+                                    className="border w-32 w-custom rounded-md p-2" />
+                            </div>
+                            <div className="flex items-center">
+                                <label htmlFor="endDate" className="font-bold text-sm mr-1">End Date:</label>
+                                <input
+                                    type="date"
+                                    size={10}
+                                    id="endDate"
+                                    value={endDate}
+                                    onChange={handleChangeEndDate}
+                                    className="border w-32 w-custom rounded-md p-2" />
+                            </div>
+
+                        </div>
+                        {/* <table className="  mt-9">
+                            <thead>
+                                <tr className="bg-gray-100">
+                                    <th className="p-4 font-bold">Transaction ID</th>
+                                    <th className="p-4 font-bold">Amount</th>
+                                    <th className="p-4 font-bold">Payment Type</th>
+                                    <th className="p-4 font-bold">Payment Status</th>
+                                    <th className="p-4 font-bold">Date</th>
+                                    <th className="p-4 font-bold">Export</th>
+                                </tr>
+                            </thead>
+                            <tbody>
+                                {filterTransactions().map((transaction, index) => (
+                                    <tr key={index} className="hover:bg-gray-50">
+                                        <td className="p-4">{transaction.id}</td>
+                                        <td className="p-4">{transaction.amount}</td>
+                                        <td className="p-4">{transaction.paymentType}</td>
+                                        <td className="p-4">{transaction.status}</td>
+                                        <td className="p-4">{transaction.date}</td>
+                                        <td className="p-4"><button className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">Export</button></td>
+                                    </tr>
+                                ))}
+                            </tbody>
+                        </table> */}
+
                         <ul className="h-64 overflow-y-auto mt-9">
                             <div className="flex justify-between">
-                                <p className="font-bold">Transaction Type</p>
+                                <p className="font-bold">Transaction ID</p>
                                 <p className="font-bold">Amount</p>
+                                <p className="font-bold">Payment Type</p>
+                                <p className="font-bold">Payment Status</p>
                                 <p className="font-bold">Date</p>
+                                <p className="font-bold">Export</p>
                             </div>
+                            <li className="flex justify-between mt-7">
+                                <span>sf</span>
+                                <span></span>
+                                <span></span>
+                            </li>
                             {filterTransactions().map((transaction, index) => (
                                 <li key={index} className="flex justify-between mt-7">
                                     <span>{transaction.description}</span>
@@ -148,10 +229,13 @@ function WalletIndex() {
                         </ul>
                     </div>
                 </div>
-
-                <Modal isOpen={isModalOpen} addAmountWallet={addAmountWallet} setAddAmountWallet={setAddAmountWallet} onClose={closeModal} onRedirect={redirectToFlutterwave} />
-                <PayLeadsForm isOpened={isModalOpen2} payLeadsForm={payLeadsForm} setPayLeadsForm={setPayLeadsForm} onClosed={closeModal2} onRedirected={redirectToPayLeadsForm} />
             </div>
+
+            <Modal isOpen={isModalOpen} addAmountWallet={addAmountWallet} setAddAmountWallet={setAddAmountWallet} onClose={closeModal} onRedirect={redirectToFlutterwave} />
+            <PayLeadsForm isOpened={isModalOpen2} payLeadsForm={payLeadsForm} setPayLeadsForm={setPayLeadsForm} onClosed={closeModal2} onRedirected={redirectToPayLeadsForm} />
+
+
+
 
 
 
