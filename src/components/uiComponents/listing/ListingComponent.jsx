76fa4ec631c3148b247316911ef08129c29cf03d
sleek-ennju <ihenachochukwudi@gmail.com>
2024-07-08 11:11:22 +0100

    adjusted featured businesses logo and added a margin top spacing on the business description

--- src/components/uiComponents/listing/ListingComponent.jsx
+++ src/components/uiComponents/listing/ListingComponent.jsx
@@ -14,10 +14,10 @@ const ListingComponent = ({logo, cover_image, org_bio, org_name, business_catego
         <div className=" flex flex-col gap-2">
             <div className="p-4 flex-1">
                 <h3 className="font-poppins font-normal text-lg text-textDark">{org_name}</h3>
-                <p className="font-poppins font-light text-sm md:text-base text-textDark">{org_bio}</p>
+                <p className="font-poppins font-light text-sm md:text-base text-textDark mt-1">{org_bio}</p>
             </div>
             <div className="border-t border-gray-200 p-4 flex items-center gap-2">
-                <div className="max-w-12 h-full overflow-hidden rounded-full">
+                <div className="w-12 h-12 overflow-hidden rounded-full">
                     <img src={logo} className="w-full h-full object-cover" alt="business logo" />
                 </div>
                 <div>
